9e712f8599f259ffe4e47f852252122efc92fe3f
ahmed040203 <ahmed.amr1201399@gmail.com>
2023-05-24 19:14:37 +0300

    edit

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,7 +3,6 @@ import department from '../models/department.js'
 import user from '../models/user.js';
 import { doctor } from './user.js';
 
-
 export const index = async (req, res) => {
     const users = await user.find({}, { number_id: 1, username: 1, email: 1, usertype: 1 }).lean();
     const subjects = await subject.find({}, { code: 1, name: 1, subject_depended: 1, doctor: 1 }).lean();
